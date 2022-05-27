0f3e848570643b8801d8697c0cf15da70c69eb3d
Salem Affes <affessalem@hotmail.fr>
2022-05-27 17:32:20 +0100

    Create apigen

---
+++ nexus/provision/auto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+api_gen() {
+    local api="$1"
+    local flags="$2"
+    local path-to-json-file="$3"
+
+}
+# api=/v1/security/privileges....
+# flags=""
+api_gen "api" "flags" "path-to-json-file"
