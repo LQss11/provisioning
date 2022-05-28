8dbad02ac8b814d4443b9cbb917b78b3b1cb7ec0
Salem Affes <affessalem@hotmail.fr>
2022-05-28 02:16:43 +0100

    Update auto script

--- nexus/provision/auto.sh
+++ nexus/provision/auto.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 api_gen() {
-    local api="$1"
-    local flags="$2"
-    local path-to-json-file="$3"
+    local request="$1"
+    local api="$2"
+    local filePath="$3"
+    #echo curl -u 'admin:stark' -X ${request} "http://localhost:8081/service/rest/${api}" -H "accept: application/json" -H "Content-Type: application/json" -d "@${json}"
+    # cat file | -d
+
+    echo curl \
+    -X "'${request}'" "'http://localhost:8081/service/rest/${api}'" \
+    -H "'accept: application/json'" -H "'Content-Type: application/json'" \
+    -u "'admin:stark'" \
+    -d "'@${filePath}'"
 
 }
+
 # api=/v1/security/privileges....
 # flags=""
-api_gen "api" "flags" "path-to-json-file"
+#api_gen "api" "request-method" "json-path"
+
+api_gen "POST" "v1/security/users" "/test/jenkins.json"
